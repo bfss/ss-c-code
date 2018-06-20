e7ec7128c0f5ac25594377cda549c2151cc6b465
bfss <bfss@bfss.com>
2018-06-20 16:42:22 +0800

    21

---
+++ 21-控制循环/loop.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+
+	/*for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			
+			printf("a\n");
+		}
+		if (i == 1)
+		{
+			break;
+		}
+		printf("b\n");
+	}*/
+
+	/*for (int i = 0; i < 5; i++)
+	{
+		if (i == 3)
+		{
+			continue;
+		}
+		
+		printf("i=%d\n", i);
+	}*/
+
+
+	/*printf("现在演示goto\n");
+	goto jump;
+	printf("我是被跳过的\n");
+
+	jump:
+	printf("我是goto跳转过来的\n");
+	*/
+
+	while (1)
+	{
+		printf("123\n");
+	}
+
+	
+	return 0;
+}
